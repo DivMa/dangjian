db63779c399650f6521316e9eba6703512f059fc
Frank <38999779+DivMa@users.noreply.github.com>
2018-11-13 10:12:28 +0800

    Add files via upload

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+  
+    indicationDots: false,
+    autoplay: true,
+    interval: 8000,
+    duration: 2000,
+    circular: true,
+    indicatoractive: "grey",
+    imgurls: ["../images/1.jpg", "../images/2.jpg", "../images/3.jpg"], 
+  },
+  //事件处理函数
+ 
+  onLoad: function () {
+
+  }
+})
